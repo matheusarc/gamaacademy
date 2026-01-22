31c59c5e9258201bac6e1add1916865213fcb166
matheusarc <matheuscostahr@gmail.com>
2026-01-22 16:21:35 -0300

    respon_02

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ const ASSETS = [
   '/validar.html',
   '/style.css',
   '/manifest.json',
+  '/assets/NR10.png',
+  '/assets/Work1.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
